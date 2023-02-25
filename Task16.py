8582553a48f330fe0a1f9b2ec4e2a95dc562ac9f
Geo-Schon <dr-gudkov@mail.ru>
2023-02-25 16:50:23 +0400

    Выполнена 16 задача

--- Task16.py
+++ Task16.py
@@ -1,10 +1,13 @@
-n = int(input('Введите число: '))
+n = int(input('Введите колиличество элементов: '))
 count = 0
-for i in range(1, n + 1):
-    print(i, end=' ')
-    if i == n:
-        count += 1
-print()
-print(count)
+lst = []
+lst.append(list(map(int, input().split())))
 
-# Не до конца понял, что за число X, поэтому сделал так
+X = int(input('Введите число X: '))
+if len (lst) != n:
+    print('Элемент не найден')
+else:
+    for i in range(1, n + 1):
+        if lst[i] == X:
+            count += 1
+    print(f'Число {X} встречается {count} раз')
